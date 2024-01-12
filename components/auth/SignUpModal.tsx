8256ed365cebc8e4ee3f0677ec857465b62278bd
허현 <nsksjsk2@hanmail.net>
2024-01-12 18:00:09 +0900

    10.1

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import styled from "styled-components";
 import CloseXIcon from "../../public/static/svg/modal/modal_colose_x_icon.svg";
 import MailIcon from "../../public/static/svg/auth/mail.svg"
@@ -6,6 +6,7 @@ import PersonIcon from "../../public/static/svg/auth/person.svg"
 import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg"
 import ClosedEyeIcon from "../../public/static/svg/auth/closed_eye.svg"
 import palette from "../../styles/palette";
+import Input from "../common/input"
 
 
 const Container = styled.form`
@@ -23,48 +24,71 @@ const Container = styled.form`
     .input-wrapper{
         position: relative;
         margin-bottom: 16px;
-        input{
-            position: relative;
-            width:100%;
-            height:46px;
-            padding: 0 44px 0 11px;
-            border: 1px solid ${palette.gray_eb};
-            border-radius:4px;
-            font-size: 16px;
-            outline:none;
-            ::placeholder{
-                color: ${palette.gray_76};
-            }
-        }
-        svg{
-            position:absolute;
-            right: 11px;
-            top: 16px;
-        }
     }
+    .sign-up-password-input-wrapper {
+        svg {
+          cursor: pointer;
+        }
+      }
 `;
 
 
 const SignUpModal: React.FC =()=>{
+    const [email, setEmail] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [firstname, setFirstname] = useState("");
+    const [password, setPassword] = useState("");
+    const [hidePassword, setHidePassword] = useState(true);
+
+    const toggleHidePassword = () => {
+        setHidePassword(!hidePassword);
+      };
+    
+    const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+          setEmail(event.target.value);
+        };
+
+    const onChangeLastname = (event: React.ChangeEvent<HTMLInputElement>) => {
+            setLastname(event.target.value);
+          };
+
+    const onChangeFirstname = (event: React.ChangeEvent<HTMLInputElement>) => {
+            setFirstname(event.target.value);
+          };
+    const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+            setPassword(event.target.value);
+          };
+
     return (
         <Container>
             {/* <CloseXIcon className="mordal-close-x-icon"/> */}
             <div className="input-wrapper">
-                <input placeholder="이메일 주소" type="email" name="email"/>
-                <MailIcon/> 
+                <input placeholder="이메일 주소" type="email" name="email" onChange={onChangeEmail}/>
+                {/* icon{<MailIcon/>} vaule={email} 이거인풋안에 들어가야함 */}
             </div>
             <div className="input-wrapper">
-                <input placeholder="이름(예:길동)"/>
-                {/* <PersonIcon/> */}
+            <input placeholder="이름(예:길동)" onChange={onChangeLastname}/>
+                {/* icon{<PersonIcon/>} vaule={Lastname} 이거인풋안에 들어가야함 */}
             </div>
             <div className="input-wrapper">
-                <input placeholder="성(예: 홍)"/>
-                {/* <PersonIcon/> */}
+            <input placeholder="성(예:홍)" onChange={onChangeFirstname}/>
+                {/* icon{<PersonIcon/>} vaule={Firstname} 이거인풋안에 들어가야함 */}
             </div>
-            <div className="input-wrapper">
-                <input placeholder="비밀번호 설정하기" type="password"/>
-                {/* <OpenedEyeIcon/> */}
-            </div>
+            <div className="input-wrapper sign-up-password-input-wrapper">
+        <Input
+          placeholder="비밀번호 설정하기"
+          type={hidePassword ? "password" : "text"}
+          icon={
+            hidePassword ? (
+              <ClosedEyeIcon onClick={toggleHidePassword} />
+            ) : (
+              <OpenedEyeIcon onClick={toggleHidePassword} />
+            )
+          }
+          value={password}
+          onChange={onChangePassword}
+          />
+          </div>
         </Container>
     )
 };
